20eaf0f8d476d0c4a741836cc3c7244169f3a463
ksimach <kshitijmachingal@gmail.com>
2026-07-31 11:34:44 -0400

    Refactor timeElapse function for countdown display
    Refactor timeElapse function to count down to a specified date and display a message when the date is reached.

--- js/functions.js
+++ js/functions.js
@@ -93,25 +93,41 @@ function startHeartAnimation() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
-	$("#elapseClock").html(result);
+    var current = new Date();
+    var seconds = Math.floor((Date.parse(date) - Date.parse(current)) / 1000);
+
+    if (seconds <= 0) {
+        $("#elapseClock").html("<span class=\"digit\">❤️ We're finally together! ❤️</span>");
+        return;
+    }
+
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    seconds = seconds % 3600;
+
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var result =
+        "<span class=\"digit\">" + days + "</span> days " +
+        "<span class=\"digit\">" + hours + "</span> hours " +
+        "<span class=\"digit\">" + minutes + "</span> minutes " +
+        "<span class=\"digit\">" + seconds + "</span> seconds";
+
+    $("#elapseClock").html(result);
 }
 
 function showMessages() {
